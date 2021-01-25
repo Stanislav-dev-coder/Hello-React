c1a293b9efa4e2032d56d11b1c36887890886039
Stanislav-dev-coder <77270852+Stanislav-dev-coder@users.noreply.github.com>
2021-01-25 17:31:00 +0300

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js', 
+  output: { 
+    path: path.resolve(__dirname, 'dist'), 
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader']
+      },
+      {
+      test: /\.(png|jpg|svg|gif)$/,
+      use: ['file-loader']
+      }
+    ]
+  }
+};
